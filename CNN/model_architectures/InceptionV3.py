b16474aaa8021b955a6b2874045b9d26f1575630
Кашкур Юрий Витальевич <KashkurYV@bsmu.by>
2021-03-05 09:46:50 +0300

    inception

--- CNN/model_architectures/InceptionV3.py
+++ CNN/model_architectures/InceptionV3.py
@@ -9,6 +9,7 @@ from tensorflow.keras.layers import (
     Conv2D,
     AveragePooling2D,
     MaxPooling2D,
+    GlobalAveragePooling2D
 )
 from tensorflow.keras.models import Model
 from tensorflow.keras.initializers import glorot_uniform
@@ -59,8 +60,30 @@ class InceptionV3:
         conv7x1c = self.conv2d(conv1x7b, 128, (7, 1))
         conv1x7c = self.conv2d(conv7x1c, 192, (1, 7))
 
-        x = concatenate([pool, conv7x1a, conv1x7c])
+        x = concatenate([conv1x1a, pool, conv7x1a, conv1x7c])
         return x
+
+    def inception_block_c(self, x):
+        conv1x1a = self.conv2d(x, 320, (1, 1))
+
+        conv1x1b = self.conv2d(x, 384, (1, 1))
+        conv1x3a = self.conv2d(conv1x1b, 384, (1, 3))
+        conv3x1a = self.conv2d(conv1x1b, 384, (3, 1))
+        concat_a = concatenate([conv1x3a, conv3x1a])
+
+        conv1x1b = self.conv2d(x, 448, (1, 1))
+        conv3x3 = self.conv2d(conv1x1b, 384, (3, 3))
+        conv3x1b = self.conv2d(conv3x3, 384, (3, 1))
+        conv1x3b = self.conv2d(conv3x3, 384, (1, 3))
+        concat_b = concatenate([conv3x1b, conv1x3b])
+
+        pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
+        pool = self.conv2d(pool, 192, (1, 1))
+        x = concatenate([conv1x1a, concat_a, concat_b, pool])
+
+        return x
+
+
 
     def reduction_block_a(self, x):
         pool = MaxPooling2D((3, 3), strides=(2, 2))(x)
@@ -72,6 +95,21 @@ class InceptionV3:
         conv3x3c = self.conv2d(conv3x3b, 96, (3, 3), strides=(2, 2), padding='valid')
 
         x = concatenate([pool, conv3x3a, conv3x3c])
+
+        return x
+
+    def reduction_block_b(self, x):
+        pool = MaxPooling2D((3, 3), (2, 2))(x)
+
+        conv1x1a = self.conv2d(x, 192, (1, 1))
+        conv3x3a = self.conv2d(conv1x1a, 320, (3, 3), strides=(2, 2), padding='valid')
+
+        conv1x1b = self.conv2d(x, 192, (1, 1))
+        conv1x7 = self.conv2d(conv1x1b, 192, (1, 7))
+        conv7x1 = self.conv2d(conv1x7, 192, (7, 1))
+        conv3x3b = self.conv2d(conv7x1, 192, (3, 3), padding='valid', strides=(2, 2))
+
+        x = concatenate(pool, conv3x3a, conv3x3b)
 
         return x
 
@@ -98,5 +136,18 @@ class InceptionV3:
         x = self.inception_block_b(x)
         x = self.inception_block_b(x)
         x = self.inception_block_b(x)
+
+        x = self.reduction_block_b(x)
+
+        x = self.inception_block_c(x)
+        x = self.inception_block_c(x)
+
+        x = GlobalAveragePooling2D(name='avg_pool')(x)
+
+        x = Dense(self._classes, activation='softmax', name='predictions')(x)
+
+        model = Model(x_input, x, name='Inception_V3')
+
+        return model
 
 
